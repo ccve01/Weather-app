aab44f5ac618d724f48f08c654c3d6f9ce7c2452
Jesus Santana <jscheveto7@gmail.com>
2023-06-28 00:57:00 +0000

    Added pandas dataframe

--- weadar.py
+++ weadar.py
@@ -16,10 +16,21 @@ print(geocode_response.json())
 lat = geocode_response.json().get('lat')
 lon = geocode_response.json().get('lon')
 
-weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly,daily&appid={API_KEY}&units=imperial"
+weather_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely,daily&appid={API_KEY}&units=imperial"
 
 weather_response = requests.get(weather_url)
 
 print("Weather")
 print(weather_response.status_code)
 print(weather_response.json())
+
+
+
+weather_df = pd.DataFrame()
+time={}
+count = 0
+for hour in weather_response.json().get('hourly'):
+    time[count] = hour 
+    count += 1
+weather_df = pd.DataFrame.from_dict(time, orient='index')
+print(weather_df)
